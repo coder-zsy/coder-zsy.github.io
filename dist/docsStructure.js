9da96f4f34f8a5cfc56b622f92b36daa319bc42f
张琦 <zqxin110@126.com>
2024-10-12 15:55:27 +0800

    feat<dist> add dist floder

---
+++ dist/docsStructure.js
@@ -0,0 +1,57 @@
+module.exports = [
+  {
+    "key": "docs/business",
+    "label": "business",
+    "type": "directory",
+    "children": [
+      {
+        "key": "docs/business/profit_review.md",
+        "label": "《盈利》作业",
+        "type": "file"
+      }
+    ]
+  },
+  {
+    "key": "docs/cognition",
+    "label": "cognition",
+    "type": "directory",
+    "children": []
+  },
+  {
+    "key": "docs/develop",
+    "label": "develop",
+    "type": "directory",
+    "children": [
+      {
+        "key": "docs/develop/linux",
+        "label": "linux",
+        "type": "directory",
+        "children": [
+          {
+            "key": "docs/develop/linux/tree.md",
+            "label": "显示目录结构",
+            "type": "file"
+          }
+        ]
+      },
+      {
+        "key": "docs/develop/web",
+        "label": "web",
+        "type": "directory",
+        "children": [
+          {
+            "key": "docs/develop/web/react_markdown.md",
+            "label": "在react中解析markdown文件",
+            "type": "file"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "docs/management",
+    "label": "management",
+    "type": "directory",
+    "children": []
+  }
+];
---
+++ dist/p__article__articleDetail.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[371],{94211:function(e,u,n){n.r(u),n.d(u,{default:function(){return t}});function t(){}}}]);
