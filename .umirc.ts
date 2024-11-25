64157309e140a7b1ddabb641015769224542fea5
张琦 <zqxin110@126.com>
2024-11-25 19:54:52 +0800

    feat:<printerMate> change privacy route

--- .umirc.ts
+++ .umirc.ts
@@ -1,53 +1,53 @@
-import { defineConfig } from "umi";
-import ScriptWebpackPlugin from "./buildScript/scriptWebpackPlugin";
+import { defineConfig } from 'umi';
+import ScriptWebpackPlugin from './buildScript/scriptWebpackPlugin';
 
-const path = require("path");
+const path = require('path');
 
 export default defineConfig({
   /**
    * github pages 里选不到dist目录，就把这里的输出目录改掉
    * */
-  outputPath: "docs",
+  outputPath: 'docs',
   alias: {
     //   "@": process.cwd() + "/src", // 定义 @ 别名指向 src 目录
-    "@": path.resolve(__dirname, "src"), // 定义 @ 别名指向 src 目录
-    "@public": path.resolve(__dirname, "public"), // 定义 @ 别名指向 src 目录
+    '@': path.resolve(__dirname, 'src'), // 定义 @ 别名指向 src 目录
+    '@public': path.resolve(__dirname, 'public'), // 定义 @ 别名指向 src 目录
   },
   routes: [
     // 错误的路由
-    { path: "/*", component: "@/pages/index" },
+    { path: '/*', component: '@/pages/index' },
     // 没有layout（layout里是空的）包裹的路由
     {
-      path: "/",
-      component: "@/layouts/index",
+      path: '/',
+      component: '@/layouts/index',
       routes: [
-        { path: "/", component: "index" },
-        { path: "/docs", component: "@/pages/docs" },
+        { path: '/', component: 'index' },
+        { path: '/docs', component: '@/pages/docs' },
       ],
     },
     {
-      path: "/printerMate",
-      component: "@/layouts/index",
+      path: '/printerMate',
+      component: '@/layouts/index',
       routes: [
         {
-          path: "/printerMate",
-          component: "@/pages/printerMate/privacyAgreement",
+          path: '/printerMate/privacyAgreement',
+          component: '@/pages/printerMate/privacyAgreement',
         },
         {
-          path: "/printerMate/userAgreement",
-          component: "@/pages/printerMate/userAgreement",
+          path: '/printerMate/userAgreement',
+          component: '@/pages/printerMate/userAgreement',
         },
       ],
     },
     // layout包裹的带菜单的路由
     {
-      path: "/article",
-      component: "@/layouts/menuLayout",
+      path: '/article',
+      component: '@/layouts/menuLayout',
       routes: [
-        { path: "/article/index", redirect: "/article/index" },
+        { path: '/article/index', redirect: '/article/index' },
         {
-          path: "/article/articleDetail",
-          component: "@/pages/article/articleDetail",
+          path: '/article/articleDetail',
+          component: '@/pages/article/articleDetail',
         },
       ],
     },
@@ -57,14 +57,14 @@ export default defineConfig({
    * umi框架下public目录中的资源会直接复制到构建输出目录，所以这里不用再复制资源文件了
    */
   // copy: [{ from: "src/xx", to: "dist/xxx" }],
-  npmClient: "pnpm",
+  npmClient: 'pnpm',
   chainWebpack: (memo) => {
-    memo.plugin("mdFloderGeneratorPlugin").use(ScriptWebpackPlugin, [
+    memo.plugin('mdFloderGeneratorPlugin').use(ScriptWebpackPlugin, [
       // { scriptPath: "./buildScript/generateDocsConfig.js" },
       {
         scriptPath: path.resolve(
           __dirname,
-          "buildScript/generateDocsConfig.js"
+          'buildScript/generateDocsConfig.js',
         ),
       },
     ]);
